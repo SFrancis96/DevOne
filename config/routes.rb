7f678d8c1c0f04d56ed55fa32337b8c400afedd4
Francis Scaringella <francis.scaringella@brassart.fr>
2022-08-10 23:19:22 +0000

    installation tailwindcss + création de la navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    root "landing_page#index"
+    get 'landing_page/index'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
