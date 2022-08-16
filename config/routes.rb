356ac30b954786e2d9efa0858d619c28bdf3dde3
Samuel A <sassani134@gmail.com>
2022-08-16 22:04:32 +0000

    Ajout du modele user généré par devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
     # Defines the root path route ("/")
     root "landing_page#index"
-    get 'landing_page/index'
+    get "/landing", to: 'landing_page#index'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
