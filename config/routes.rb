021f31dad6d7aeecf4a0d52c625598ac055bcb65
Francis Scaringella <francis.scaringella@brassart.fr>
2022-08-21 20:24:28 +0000

    redirection après sign_up, good

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'welcome/index'
+    devise_for :users
+    get '/welcome' => "welcome#index", as: :user_root
     # Defines the root path route ("/")
     root "landing_page#index"
     get "/landing", to: 'landing_page#index'
